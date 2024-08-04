027e0dca95317857650284430cc3c219d6621a72
Scarlet Gonzalez <smgonzalez@neural.com.do>
2024-08-04 10:12:05 -0400

    Complete edit modal for ciencias

--- biblioteca/src/main/java/com/sg/biblioteca/services/CienciasService.java
+++ biblioteca/src/main/java/com/sg/biblioteca/services/CienciasService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sg.biblioteca.models.Ciencias;
+import com.sg.biblioteca.models.Ciencias;
 import com.sg.biblioteca.repositories.CienciasRepository;
 
 @Service
@@ -14,9 +15,33 @@ public class CienciasService {
 	@Autowired
 	   private CienciasRepository cienciasRepository;
 		
-		 //Return list of autores
+		 //Return list of ciencia
 		public List<Ciencias> ListCiencias(){
 			return cienciasRepository.findAll();
 		}
+		
+		public Ciencias getCienciaById(Integer id) {
+	        return cienciasRepository.findById(id).orElse(null);
+	    }
+		
+		
+		//Save a new ciencia
+		public void saveNew(Ciencias ciencia) {
+			cienciasRepository.save(ciencia);
+		}
+
+	    public void updateCiencia(Ciencias ciencia) {
+	        Ciencias existingCiencia = cienciasRepository.findById(ciencia.getId()).orElse(null);
+	        if (existingCiencia != null) {
+	            existingCiencia.setDescripcion(ciencia.getDescripcion());
+	            existingCiencia.setActivo(ciencia.isActivo());
+	            cienciasRepository.save(existingCiencia);
+	        }
+	    }
+
+	    public void deleteCiencia(Integer id) {
+	        cienciasRepository.deleteById(id);
+	    }
+		
 
 }
