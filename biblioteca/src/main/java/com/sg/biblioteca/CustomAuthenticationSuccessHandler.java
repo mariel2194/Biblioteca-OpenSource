b566fe4a2d424891e8afd2f9e01d9e7533d53999
Scarlet Gonzalez <smgonzalez@neural.com.do>
2024-08-06 19:28:51 -0400

    complete login

--- biblioteca/src/main/java/com/sg/biblioteca/CustomAuthenticationSuccessHandler.java
+++ biblioteca/src/main/java/com/sg/biblioteca/CustomAuthenticationSuccessHandler.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import java.io.IOException;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
