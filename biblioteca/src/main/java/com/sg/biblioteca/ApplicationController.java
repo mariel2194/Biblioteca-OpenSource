581fee4def8e4320cdd99538524810d3af8502e7
Scarlet Gonzalez <smgonzalez@neural.com.do>
2024-08-07 21:00:27 -0400

    Refactor views

--- biblioteca/src/main/java/com/sg/biblioteca/ApplicationController.java
+++ biblioteca/src/main/java/com/sg/biblioteca/ApplicationController.java
@@ -1,63 +1,70 @@
-package com.sg.biblioteca;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.sg.biblioteca.models.Libros;
-import com.sg.biblioteca.models.Prestamos;
-import com.sg.biblioteca.models.Usuarios;
-import com.sg.biblioteca.services.LibrosService;
-import com.sg.biblioteca.services.PrestamosService;
-import com.sg.biblioteca.services.UsuariosService;
-
-@Controller
-public class ApplicationController {
-
-	@Autowired
-	private PrestamosService prestamosService;
-	@Autowired
-	private UsuariosService usuariosService;
-	@Autowired
-	private LibrosService librosService;
-
-	@GetMapping("/index")
-	public String goHome(Model model) {
-		List<Prestamos> prestamosList = prestamosService.ListPrestamos();
-		List<Usuarios> usuariosList = usuariosService.ListUsuarios();
-		List<Libros> librosList = librosService.ListLibros();
-
-		Map<Integer, Boolean> devolucionesExistentes = new HashMap<>();
-		for (Prestamos prestamo : prestamosList) {
-			devolucionesExistentes.put(prestamo.getId(), prestamosService.tieneDevolucion(prestamo.getId()));
+	package com.sg.biblioteca;
+	
+	import java.util.Arrays;
+	import java.util.HashMap;
+	import java.util.List;
+	import java.util.Map;
+	
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.ui.Model;
+	import org.springframework.web.bind.annotation.GetMapping;
+	
+	import com.sg.biblioteca.models.Libros;
+	import com.sg.biblioteca.models.Empleados;
+	import com.sg.biblioteca.models.Prestamos;
+	import com.sg.biblioteca.models.Usuarios;
+	import com.sg.biblioteca.services.EmpleadosService;
+	import com.sg.biblioteca.services.LibrosService;
+	import com.sg.biblioteca.services.PrestamosService;
+	import com.sg.biblioteca.services.UsuariosService;
+	
+	@Controller
+	public class ApplicationController {
+	
+		@Autowired
+		private PrestamosService prestamosService;
+		@Autowired
+		private UsuariosService usuariosService;
+		@Autowired
+		private LibrosService librosService;
+		@Autowired
+		private EmpleadosService empleadosService;
+	
+		@GetMapping("/index")
+		public String goHome(Model model) {
+			List<Prestamos> prestamosList = prestamosService.ListPrestamos();
+			List<Usuarios> usuariosList = usuariosService.ListUsuarios();
+			List<Libros> librosList = librosService.ListLibros();
+			List<Empleados> empleadosList = empleadosService.ListEmpleados();
+	
+	
+			Map<Integer, Boolean> devolucionesExistentes = new HashMap<>();
+			for (Prestamos prestamo : prestamosList) {
+				devolucionesExistentes.put(prestamo.getId(), prestamosService.tieneDevolucion(prestamo.getId()));
+			}
+			model.addAttribute("devolucionesExistentes", devolucionesExistentes);
+			model.addAttribute("libros", librosList);
+	
+			// Lista de URLs de imágenes
+			List<String> imagenes = Arrays.asList("/assets/img/img1.jpg", "/assets/img/img2.jpg", "/assets/img/img3.jpg",
+					"/assets/img/img4.jpg");
+			model.addAttribute("imagenes", imagenes);
+			model.addAttribute("usuarios", usuariosList);
+			model.addAttribute("prestamos", prestamosList);
+			model.addAttribute("empleados", empleadosList);
+	
+			return "index";
 		}
-		model.addAttribute("devolucionesExistentes", devolucionesExistentes);
-		model.addAttribute("libros", librosList);
-
-		// Lista de URLs de imágenes
-		List<String> imagenes = Arrays.asList("/assets/img/img1.jpg", "/assets/img/img2.jpg", "/assets/img/img3.jpg",
-				"/assets/img/img4.jpg");
-		model.addAttribute("imagenes", imagenes);
-		model.addAttribute("usuarios", usuariosList);
-		model.addAttribute("prestamos", prestamosList);
-
-		return "index";
+	
+		@GetMapping("/login")
+		public String login() {
+			return "login";
+		}
+	
+		@GetMapping("/logout")
+		public String logout() {
+			return "login";
+		}
+	
 	}
-
-	@GetMapping("/login")
-	public String login() {
-		return "login";
-	}
-
-	@GetMapping("/logout")
-	public String logout() {
-		return "login";
-	}
-
-}
